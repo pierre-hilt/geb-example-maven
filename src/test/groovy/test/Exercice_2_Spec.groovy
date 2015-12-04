a3b195b940f031af51d92c4fedd0454704f90f53
philt <pierre.hilt@amadeus.com>
2015-12-04 14:57:59 +0100

    Add TODO proto and new exercises

---
+++ src/test/groovy/test/Exercice_2_Spec.groovy
@@ -0,0 +1,10 @@
+package test
+
+import geb.spock.GebReportingSpec
+import page.GebishOrgHomePage
+
+class Exercice_2_Spec extends GebReportingSpec {
+
+  def "Interact with a form to create a task"() {
+  }
+}
---
+++ src/test/groovy/test/Exercice_3_Spec.groovy
@@ -0,0 +1,12 @@
+package test
+
+import geb.spock.GebReportingSpec
+
+class Exercice_3_Spec extends GebReportingSpec {
+
+  def "Access a span displayed when a button is clicked"() {
+  }
+
+  def "Access a span displayed when a button is clicked after a tiemout"() {
+  }
+}
