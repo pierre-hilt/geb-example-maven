2674d05afdbf1d3b67a7a90caea1cf898677c06d
Pierre Hilt <pierre.hilt@gmail.com>
2015-12-07 00:09:49 +0100

    Do exercise 1

--- src/test/groovy/test/Exercise_1_Spec.groovy
+++ src/test/groovy/test/Exercise_1_Spec.groovy
@@ -8,23 +8,29 @@ class Exercise_1_Spec extends BaseSpec {
 
   def "Can get to Gebish webstite and check UI"() {
     expect: 'Six links displayed in Higlights'
-      4 == 6
+      $(".sidemenu a").size() == 6
     and: 'Text of the highlight section is "Highlights"'
-      "Highlts" == "Highlights"
+      $("#sidebar h1").text() == "Highlights"
   }
 
   def "Can get to Crossbrowser page from highlights"() {
     when: "Click on Cross Browser Link"
+      $("#sidebar a", text: "Cross Browser").click()
     then: "Cross Browser page is displayed"
-      false
+      $("#main h1").text() == "Cross Browser Automation"
   }
 
   def "Can get to Manual section"() {
     when: "Hover Manual menu"
+      interact {
+        moveToElement($("#header-content span", text:'MANUAL'))
+      }
     then: "Manual menu is displayed"
-      "Highlts" == "Highlights"
+      $('.link-list', 0).displayed
 
     when: "Click on current item in menu"
+      $('.link-list', 0).find("a", 0).click()
     then: "Manual page is displayed"
+      title.startsWith("The Book Of Geb")
   }
 }
