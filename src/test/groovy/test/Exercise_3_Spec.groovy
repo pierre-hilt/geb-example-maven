b1eacea3326ecbf56cdebbe284b16e87ec265b55
Pierre Hilt <pierre.hilt@gmail.com>
2015-12-07 00:51:22 +0100

    Todo with timeout

--- src/test/groovy/test/Exercise_3_Spec.groovy
+++ src/test/groovy/test/Exercise_3_Spec.groovy
@@ -1,5 +1,8 @@
 package test
 
+import org.openqa.selenium.Keys
+import page.TodoPage
+
 class Exercise_3_Spec extends BaseSpec {
 
   def setupSpec() {
@@ -7,8 +10,14 @@ class Exercise_3_Spec extends BaseSpec {
   }
 
   def "Create a new task"() {
+    setup:
+      TodoPage todoPage = at TodoPage
     when: "Enter some value in the input"
+      todoPage.input << "This is task"
     and: "Send enter key to create task"
+      todoPage.input << Keys.ENTER
     then: "Task is created"
+      todoPage.todoList.size() == 1
+      todoPage.todoList(0).task == "This is task" // add wait: true in the content options
   }
 }
